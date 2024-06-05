5c9894c88116d82c396d2dfd986ebf13d68e1354
Omar Fathi22 <ommarf525@gmail.com>
2024-06-05 12:19:56 -0700

    fix error

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,14 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - Goes through a binary tree using level-order
- *                           traversal
+ * binary_tree_levelorder - Traverses a binary tree using level-order
+ * traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  *
- * Description: If `tree` or `func` is NULL, do nothing
+ * Description: This function traverses the binary tree using level-order
+ * traversal and calls the given function for each visited node. The value in
+ * each node is passed as a parameter to the given function.
  */
-
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
