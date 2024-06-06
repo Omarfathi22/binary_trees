435cc269b2e90ee9bc78047c74fbaa8c7c03def3
Omar Fathi22 <ommarf525@gmail.com>
2024-06-06 11:36:25 -0700

    124-sorted_array_to_avl.c

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * sorted_array_to_avl_helper - Helper function to build AVL tree from sorted array
+ * @array: Pointer to the first element of the array to be converted
+ * @start: Starting index of the subarray
+ * @end: Ending index of the subarray
+ * @parent: Pointer to the parent node
+ *
+ * Return: Pointer to the root node of the created AVL subtree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl_helper(int *array, int start, int end, avl_t *parent)
+{
+    if (start > end)
+        return (NULL);
+
+    int mid = (start + end) / 2;
+    avl_t *node = binary_tree_node(parent, array[mid]);
+    if (!node)
+        return (NULL);
+
+    node->left = sorted_array_to_avl_helper(array, start, mid - 1, node);
+    node->right = sorted_array_to_avl_helper(array, mid + 1, end, node);
+
+    return (node);
+}
+
+/**
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    if (!array || size == 0)
+        return (NULL);
+
+    return (sorted_array_to_avl_helper(array, 0, size - 1, NULL));
+}
